04a45fcbe5ae364f5716b9602e0fd069f050a04c
Alejandro Gómez Orjuela <102133299+alejoDev117@users.noreply.github.com>
2023-04-30 16:43:34 -0500

    se crean varios Entity

---
+++ src/co/edu/uco/compuconnect/entities/TipoNotificacionEntity.java
@@ -0,0 +1,69 @@
+package co.edu.uco.compuconnect.entities;
+
+import java.util.UUID;
+
+import co.edu.uco.compuconnect.crosscutting.utils.UtilText;
+import co.edu.uco.compuconnect.crosscutting.utils.UtilUUID;
+
+
+public class TipoNotificacionEntity {
+	
+	
+	private static final TipoNotificacionEntity DEFAULT_OBJECT = new TipoNotificacionEntity();
+	private UUID identificador;
+	private String nombre;
+	private String descripcion;
+	
+	
+	public TipoNotificacionEntity(UUID identificador, String nombre, String descripcion) {
+		super();
+		setIdentificador(identificador);
+		setNombre(nombre);
+		setDescripcion(descripcion);
+	}
+	
+	private TipoNotificacionEntity() { 
+		super();
+		setIdentificador(UtilUUID.getDefaultUuid());
+		setNombre(UtilText.getDefaultValue());
+		setDescripcion(UtilText.getDefaultValue());
+	}
+	
+	
+	
+	
+	public static final TipoNotificacionEntity getDefaultObject() {
+		return DEFAULT_OBJECT;
+	}
+	
+	
+
+	public final UUID getIdentificador() {
+		return identificador;
+	}
+	
+	private final void setIdentificador(final UUID identificador) {
+		this.identificador = UtilUUID.getDefault(identificador);
+		
+	}
+	public final String getNombre() {
+		return nombre;
+	}
+	
+	private final void setNombre(final String nombre) {
+		this.nombre = UtilText.applyTrim(nombre) ;
+		
+	}
+	
+	public final String getDescripcion() {
+		return descripcion;
+	}
+	
+	private final void setDescripcion(final String descripcion) {
+		this.descripcion = UtilText.applyTrim(descripcion);
+		
+	}
+	
+	
+
+}
